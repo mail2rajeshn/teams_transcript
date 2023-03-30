77c52e851e4f79a9294bc81acc40c38426b2b129
Rasa <mail2rajeshn@gmail.com>
2023-03-30 11:49:57 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,7 +33,7 @@ c = st.container()
 
 
 st.markdown('Streamlit is **_really_ cool**.')
-st.markdown(”This text is :red[colored red], and this is **:blue[colored]** and bold.”)
+st.markdown("This text is :red[colored red], and this is **:blue[colored]** and bold.")
 st.markdown(":green[$\sqrt{x^2+y^2}=1$] is a Pythagorean identity. :pencil:")
 
 
